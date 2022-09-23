a4d0642bf353510c1543bdd5a56d6063ce51bd11
Bruno Henrique <brunohqsantos@hotmail.com>
2022-09-22 22:23:13 -0300

    ADD: Config fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import {App} from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1'
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
